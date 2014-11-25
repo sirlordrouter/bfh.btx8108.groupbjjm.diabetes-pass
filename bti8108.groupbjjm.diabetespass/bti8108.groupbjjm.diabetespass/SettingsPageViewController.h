971171e30b962f6debaf305ac9cc3c39535d586c
Saskia Basler <basls1@BTX8108a-Workplace-A.local>
2014-11-25 22:14:26 +0100

    DatePickers in SettingsView

--- bti8108.groupbjjm.diabetespass/bti8108.groupbjjm.diabetespass/SettingsPageViewController.h
+++ bti8108.groupbjjm.diabetespass/bti8108.groupbjjm.diabetespass/SettingsPageViewController.h
@@ -14,11 +14,40 @@
 @property IBOutlet UITableViewCell *morningCell;
 @property IBOutlet UIDatePicker *morningDatePicker;
 @property IBOutlet UITableViewCell *morningDatePickerCell;
+@property (strong, nonatomic) NSDateFormatter *morningDateFormatter;
+@property (strong, nonatomic) NSDate *selectedMorningTime;
 
-@property (nonatomic, assign, getter=isDateOpen) BOOL dateOpen;
+@property IBOutlet UITableViewCell *middayCell;
+@property IBOutlet UIDatePicker *middayDatePicker;
+@property IBOutlet UITableViewCell *middayDatePickerCell;
+@property (strong, nonatomic) NSDateFormatter *middayDateFormatter;
+@property (strong, nonatomic) NSDate *selectedMiddayTime;
+
+@property IBOutlet UITableViewCell *eveningCell;
+@property IBOutlet UIDatePicker *eveningDatePicker;
+@property IBOutlet UITableViewCell *eveningDatePickerCell;
+@property (strong, nonatomic) NSDateFormatter *eveningDateFormatter;
+@property (strong, nonatomic) NSDate *selectedEveningTime;
+
+@property IBOutlet UITableViewCell *nightCell;
+@property IBOutlet UIDatePicker *nightDatePicker;
+@property IBOutlet UITableViewCell *nightDatePickerCell;
+@property (strong, nonatomic) NSDateFormatter *nightDateFormatter;
+@property (strong, nonatomic) NSDate *selectedNightTime;
+
+@property (assign) BOOL datePickerIsShowing;
+
+@property (nonatomic, assign, getter=isMorningDateOpen) BOOL morningDateOpen;
+@property (nonatomic, assign, getter=isMiddayDateOpen) BOOL middayDateOpen;
+@property (nonatomic, assign, getter=isEveningDateOpen) BOOL eveningDateOpen;
+@property (nonatomic, assign, getter=isNightDateOpen) BOOL nightDateOpen;
 
 @property IBOutlet UILabel *typLabel;
 @property IBOutlet UILabel *therapieLabel;
+@property IBOutlet UILabel *morningtimeLabel;
+@property IBOutlet UILabel *middaytimeLabel;
+@property IBOutlet UILabel *eveningtimeLabel;
+@property IBOutlet UILabel *nighttimeLabel;
 
 
 
--- bti8108.groupbjjm.diabetespass/bti8108.groupbjjm.diabetespass/SettingsPageViewController.m
+++ bti8108.groupbjjm.diabetespass/bti8108.groupbjjm.diabetespass/SettingsPageViewController.m
@@ -9,10 +9,14 @@
 #import "SettingsPageViewController.h"
 #import "SettingsTypPageViewController.h"
 #import "SettingsTherapyPageViewController.h"
-#import "NotfallViewController.h"
+
+#define kDatePickerIndex 2
+#define kDatePickerCellHeight 164
 
 
 @interface SettingsPageViewController ()
+
+
 
 
 @end
@@ -23,6 +27,9 @@
 @synthesize morningDatePickerCell;
 @synthesize typLabel;
 @synthesize therapieLabel;
+@synthesize middayCell;
+@synthesize eveningCell;
+@synthesize nightCell;
 
 - (IBAction)unwindToEinstellungen1:(UIStoryboardSegue *)unwindSegue
 {
@@ -30,7 +37,7 @@
     
     if ([sourceViewController isKindOfClass:[SettingsTypPageViewController class]])
     {
-    
+        
         NSLog(@"Diabetes Typ 1");
         typLabel.text = @"Diabetes Typ 1";
     }
@@ -39,7 +46,7 @@
         NSLog(@"ICT");
         therapieLabel.text = @"ICT";
     }
-
+    
 }
 
 - (IBAction)unwindToEinstellungen2:(UIStoryboardSegue *)unwindSegue
@@ -52,20 +59,30 @@
         NSLog(@"Diabetes Typ 2");
         typLabel.text = @"Diabetes Typ 2";
     }
-        else if ([sourceViewController isKindOfClass:[SettingsTherapyPageViewController class]])
-        {
-            NSLog(@"konventionelle Therapieform");
-            therapieLabel.text = @"konventionell";
-        }
+    else if ([sourceViewController isKindOfClass:[SettingsTherapyPageViewController class]])
+    {
+        NSLog(@"konventionelle Therapieform");
+        therapieLabel.text = @"konventionell";
+    }
     
-
+    
 }
+
 
 - (void)viewDidLoad {
     
     [super viewDidLoad];
-    [morningDatePicker setHidden:(YES)];
-
+    
+    [self setupMorningLabel];
+    [self setupMiddayLabel];
+    [self setupEveningLabel];
+    [self setupNightLabel];
+    
+    [self.morningDatePicker setHidden:(YES)];
+    [self.middayDatePicker setHidden:(YES)];
+    [self.eveningDatePicker setHidden:(YES)];
+    [self.nightDatePicker setHidden:(YES)];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -73,98 +90,377 @@
     [super didReceiveMemoryWarning];
 }
 
+#pragma mark - Helper methods
 
--(CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+- (void)setupMorningLabel {
     
-    // This is the index path of the date picker cell in the static table
-    if (indexPath.section == 1 && indexPath.row == 1 && !self.isDateOpen){
+    self.morningDateFormatter = [[NSDateFormatter alloc] init];
+    [self.morningDateFormatter setDateStyle:NSDateFormatterNoStyle];
+    [self.morningDateFormatter setTimeStyle:NSDateFormatterShortStyle];
+    [self.morningDateFormatter setDateFormat:@"HH:mm"];
+    
+    NSDate *defaultDate = [NSDate date];
+    
+    //    self.morningtimeLabel.text = [self.dateFormatter stringFromDate:defaultDate];
+    self.morningtimeLabel.textColor = [self.tableView tintColor];
+    
+    self.selectedMorningTime = defaultDate;
+}
+
+- (void)setupMiddayLabel {
+    
+    self.middayDateFormatter = [[NSDateFormatter alloc] init];
+    [self.middayDateFormatter setDateStyle:NSDateFormatterNoStyle];
+    [self.middayDateFormatter setTimeStyle:NSDateFormatterShortStyle];
+    [self.middayDateFormatter setDateFormat:@"HH:mm"];
+    
+    NSDate *defaultDate = [NSDate date];
+    
+    //    self.morningtimeLabel.text = [self.dateFormatter stringFromDate:defaultDate];
+    self.middaytimeLabel.textColor = [self.tableView tintColor];
+    
+    self.selectedMiddayTime = defaultDate;
+}
+
+- (void)setupEveningLabel {
+    
+    self.eveningDateFormatter = [[NSDateFormatter alloc] init];
+    [self.eveningDateFormatter setDateStyle:NSDateFormatterNoStyle];
+    [self.eveningDateFormatter setTimeStyle:NSDateFormatterShortStyle];
+    [self.eveningDateFormatter setDateFormat:@"HH:mm"];
+    
+    NSDate *defaultDate = [NSDate date];
+    
+    //    self.morningtimeLabel.text = [self.dateFormatter stringFromDate:defaultDate];
+    self.eveningtimeLabel.textColor = [self.tableView tintColor];
+    
+    self.selectedEveningTime = defaultDate;
+}
+
+- (void)setupNightLabel {
+    
+    self.nightDateFormatter = [[NSDateFormatter alloc] init];
+    [self.nightDateFormatter setDateStyle:NSDateFormatterNoStyle];
+    [self.nightDateFormatter setTimeStyle:NSDateFormatterShortStyle];
+    [self.nightDateFormatter setDateFormat:@"HH:mm"];
+    
+    NSDate *defaultDate = [NSDate date];
+    
+    //    self.morningtimeLabel.text = [self.dateFormatter stringFromDate:defaultDate];
+    self.nighttimeLabel.textColor = [self.tableView tintColor];
+    
+    self.selectedNightTime = defaultDate;
+}
+
+
+
+#pragma mark - Table view methods
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    //  This is the index path of the date picker cell in the static table
+    if (indexPath.section == 1 && indexPath.row == 1 && !self.isMorningDateOpen){
+        return 0;
+    }
+    if (indexPath.section == 1 && indexPath.row == 3 && !self.isMiddayDateOpen){
+        return 0;
+    }
+    else if (indexPath.section == 1 && indexPath.row == 5 && !self.isEveningDateOpen){
+        return 0;
+    }
+    else if (indexPath.section == 1 && indexPath.row == 7 && !self.isNightDateOpen){
         return 0;
     }
     return [super tableView:tableView heightForRowAtIndexPath:indexPath];
 }
 
--(void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell* cell = [tableView cellForRowAtIndexPath:indexPath];
-    [tableView beginUpdates];
-    
-    if (cell == self.morningCell){
-        self.dateOpen = !self.isDateOpen;
-        [morningDatePicker setHidden:(NO)];
-
+    if (cell == morningCell){
+        self.morningDateOpen = !self.isMorningDateOpen;
+        
+        if (self.datePickerIsShowing){
+            
+            [self hideMorningDatePickerCell];
+            
+        }else {
+            
+            
+            [self showMorningDatePickerCell];
+        }
     }
+    
+    else if (cell == middayCell){
+        self.middayDateOpen = !self.isMiddayDateOpen;
+        
+        if (self.datePickerIsShowing){
+            
+            [self hideMiddayDatePickerCell];
+            
+        }else {
+            
+            
+            [self showMiddayDatePickerCell];
+        }
+    }
+    
+    else if (cell == eveningCell){
+        self.eveningDateOpen = !self.isEveningDateOpen;
+        
+        if (self.datePickerIsShowing){
+            
+            [self hideEveningDatePickerCell];
+            
+        }else {
+            
+            
+            [self showEveningDatePickerCell];
+        }
+    }
+    
+    else if (cell == nightCell){
+        self.nightDateOpen = !self.isNightDateOpen;
+        
+        if (self.datePickerIsShowing){
+            
+            [self hideNightDatePickerCell];
+            
+        }else {
+            
+            
+            [self showNightDatePickerCell];
+        }
+    }
+    
+    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
 
-  
+- (void)showMorningDatePickerCell {
+    
+    self.datePickerIsShowing = YES;
+    
+    [self.tableView beginUpdates];
+    
     [self.tableView endUpdates];
-}
-
-//-(void) tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath{
-//    UITableViewCell* cell = [tableView cellForRowAtIndexPath:indexPath];
-//    [tableView beginUpdates];
-//    
-//    if (cell == self.morningCell){
-//        self.dateOpen = !self.isDateOpen;
-//        [morningDatePicker setHidden:(YES)];
-//
-//        
-//    }
-//    
-//      [self.tableView endUpdates];
-//}
-
-
-
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
     
-    // Configure the cell...
+    self.morningDatePicker.hidden = NO;
+    self.morningDatePicker.alpha = 0.0f;
     
-    return cell;
+    [UIView animateWithDuration:0.25 animations:^{
+        
+        self.morningDatePicker.alpha = 1.0f;
+        
+    }];
 }
-*/
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+- (void)hideMorningDatePickerCell {
+    
+    self.datePickerIsShowing = NO;
+    
+    [self.tableView beginUpdates];
+    [self.tableView endUpdates];
+    
+    [UIView animateWithDuration:0.25
+                     animations:^{
+                         self.morningDatePicker.alpha = 0.0f;
+                     }
+                     completion:^(BOOL finished){
+                         self.morningDatePicker.hidden = YES;
+                     }];
 }
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+- (void)showMiddayDatePickerCell {
+    
+    self.datePickerIsShowing = YES;
+    
+    [self.tableView beginUpdates];
+    
+    [self.tableView endUpdates];
+    
+    self.middayDatePicker.hidden = NO;
+    self.middayDatePicker.alpha = 0.0f;
+    
+    [UIView animateWithDuration:0.25 animations:^{
+        
+        self.middayDatePicker.alpha = 2.0f;
+        
+    }];
 }
-*/
 
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+- (void)hideMiddayDatePickerCell {
+    
+    self.datePickerIsShowing = NO;
+    
+    [self.tableView beginUpdates];
+    [self.tableView endUpdates];
+    
+    [UIView animateWithDuration:0.25
+                     animations:^{
+                         self.middayDatePicker.alpha = 1.0f;
+                     }
+                     completion:^(BOOL finished){
+                         self.middayDatePicker.hidden = YES;
+                     }];
 }
-*/
 
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
+- (void)showEveningDatePickerCell {
+    
+    self.datePickerIsShowing = YES;
+    
+    [self.tableView beginUpdates];
+    
+    [self.tableView endUpdates];
+    
+    self.eveningDatePicker.hidden = NO;
+    self.eveningDatePicker.alpha = 0.0f;
+    
+    [UIView animateWithDuration:0.25 animations:^{
+        
+        self.eveningDatePicker.alpha = 1.0f;
+        
+    }];
 }
-*/
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)hideEveningDatePickerCell {
+    
+    self.datePickerIsShowing = NO;
+    
+    [self.tableView beginUpdates];
+    [self.tableView endUpdates];
+    
+    [UIView animateWithDuration:0.25
+                     animations:^{
+                         self.eveningDatePicker.alpha = 0.0f;
+                     }
+                     completion:^(BOOL finished){
+                         self.eveningDatePicker.hidden = YES;
+                     }];
 }
-*/
+
+- (void)showNightDatePickerCell {
+    
+    self.datePickerIsShowing = YES;
+    
+    [self.tableView beginUpdates];
+    
+    [self.tableView endUpdates];
+    
+    self.nightDatePicker.hidden = NO;
+    self.nightDatePicker.alpha = 0.0f;
+    
+    [UIView animateWithDuration:0.25 animations:^{
+        
+        self.nightDatePicker.alpha = 1.0f;
+        
+    }];
+}
+
+- (void)hideNightDatePickerCell {
+    
+    self.datePickerIsShowing = NO;
+    
+    [self.tableView beginUpdates];
+    [self.tableView endUpdates];
+    
+    [UIView animateWithDuration:0.25
+                     animations:^{
+                         self.nightDatePicker.alpha = 0.0f;
+                     }
+                     completion:^(BOOL finished){
+                         self.nightDatePicker.hidden = YES;
+                     }];
+}
+
+#pragma mark - UITextFieldDelegate methods
+
+
+#pragma mark - Action methods
+
+- (IBAction)pickerMorningDateChanged:(UIDatePicker *)sender {
+    
+    self.morningtimeLabel.text =  [self.morningDateFormatter stringFromDate:sender.date];
+    
+    self.selectedMorningTime = sender.date;
+}
+
+- (IBAction)pickerMiddayDateChanged:(UIDatePicker *)sender {
+    
+    self.middaytimeLabel.text =  [self.middayDateFormatter stringFromDate:sender.date];
+    
+    self.selectedMiddayTime = sender.date;
+}
+
+- (IBAction)pickerEveningDateChanged:(UIDatePicker *)sender {
+    
+    self.eveningtimeLabel.text =  [self.eveningDateFormatter stringFromDate:sender.date];
+    
+    self.selectedEveningTime = sender.date;
+}
+
+- (IBAction)pickerNightDateChanged:(UIDatePicker *)sender {
+    
+    self.nighttimeLabel.text =  [self.nightDateFormatter stringFromDate:sender.date];
+    
+    self.selectedNightTime = sender.date;
+}
+
 
 @end
+
+
+
+
+/*
+ - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+ UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+ 
+ // Configure the cell...
+ 
+ return cell;
+ }
+ */
+
+/*
+ // Override to support conditional editing of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the specified item to be editable.
+ return YES;
+ }
+ */
+
+/*
+ // Override to support editing the table view.
+ - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+ if (editingStyle == UITableViewCellEditingStyleDelete) {
+ // Delete the row from the data source
+ [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+ } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+ // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+ }
+ }
+ */
+
+/*
+ // Override to support rearranging the table view.
+ - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+ }
+ */
+
+/*
+ // Override to support conditional rearranging of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the item to be re-orderable.
+ return YES;
+ }
+ */
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
