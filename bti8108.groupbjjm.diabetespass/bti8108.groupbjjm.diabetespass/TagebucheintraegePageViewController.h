db61822cb9cda8c31fd453409263ac50f4b3c833
Johannes Gnägi <johannes3004@gmail.com>
2015-01-06 13:12:25 +0100

    tagebuch entry

---
+++ bti8108.groupbjjm.diabetespass/bti8108.groupbjjm.diabetespass/TagebucheintraegePageViewController.h
@@ -0,0 +1,49 @@
+//
+//  TagebucheintraegePageViewController.h
+//  bti8108.groupbjjm.diabetespass
+//
+//  Created by Meryam M on 27.11.14.
+//  Edited by Johannes Gnägi on 05-01-2015
+//  Copyright (c) 2014 Berner Fachhochschule. All rights reserved.
+//
+
+#ifndef bti8108_groupbjjm_diabetespass_TagebucheintraegePageViewController_h
+#define bti8108_groupbjjm_diabetespass_TagebucheintraegePageViewController_h
+
+
+#endif
+
+#import <UIKit/UIKit.h>
+#import "WeightPickerView.h"
+
+@interface TagebucheintraegePageViewController : UITableViewController<UITableViewDataSource, UITableViewDelegate>
+
+
+@property IBOutlet UITableViewCell *DatumCell;
+@property IBOutlet UIDatePicker *DatumDatePicker;
+@property IBOutlet UITableViewCell *DatumDatePickerCell;
+@property (strong, nonatomic) NSDateFormatter *DatumDateFormatter;
+@property (strong, nonatomic) NSDate *selectedDatumDate;
+
+@property (assign) BOOL datePickerIsShowing;
+@property (assign) BOOL PickerViewIsShowing;
+
+@property (nonatomic, assign, getter=isDatumDateOpen) BOOL DatumDateOpen;
+
+
+@property IBOutlet UITableViewCell *GewichtCell;
+@property IBOutlet WeightPickerView *GewichtPickerView;
+@property IBOutlet UITableViewCell *GewichtPickerViewCell;
+
+@property IBOutlet UILabel *DatumDateLabel;
+@property IBOutlet UILabel *GewichtLabel;
+@property IBOutlet UITextField *GlucoseField;
+@property IBOutlet UITextField *BlutzuckerField;
+@property IBOutlet UITextField *HbA1cField;
+
+@property (strong, nonatomic) IBOutlet UITextField *textField;
+@property(nonatomic, readonly, getter=isEditing) BOOL editing;
+
+
+
+@end
---
+++ bti8108.groupbjjm.diabetespass/bti8108.groupbjjm.diabetespass/TagebucheintraegePageViewController.m
@@ -0,0 +1,196 @@
+//
+//  TagebucheintraegePageViewController.m
+//  bti8108.groupbjjm.diabetespass
+//
+//  Created by Meryam M on 27.11.14.
+//  Copyright (c) 2014 Berner Fachhochschule. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TagebucheintraegePageViewController.h"
+
+/// Weight default value.
+#define WEIGHT_DEFAULT_VALUE @"75.0"
+
+@interface TagebucheintraegePageViewController ()
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *saveButton;
+@end
+
+@interface UITapGestureRecognizer ()
+
+@end
+
+
+@implementation TagebucheintraegePageViewController
+@synthesize DatumCell;
+@synthesize DatumDatePicker;
+@synthesize DatumDatePickerCell;
+
+@synthesize GewichtCell;
+@synthesize GewichtPickerView;
+@synthesize GewichtPickerViewCell;
+
+@synthesize GlucoseField;
+@synthesize BlutzuckerField;
+@synthesize HbA1cField;
+@synthesize GewichtLabel;
+
+
+    - (void)viewDidLoad {
+        
+        [super viewDidLoad];
+        
+        self.tableView.dataSource = self;
+        self.tableView.delegate = self;
+        
+        [self setupDatumLabel];
+        
+        [self.DatumDatePicker setHidden:(YES)];
+        
+        self.GewichtPickerView = [[WeightPickerView alloc] init];
+        [self.GewichtPickerViewCell addSubview:self.GewichtPickerView];
+        
+        self.GewichtPickerView.textField = GewichtLabel;
+        
+        [self.GewichtPickerView setValue:74.0];
+        
+        [self.GewichtPickerView setHidden:YES];
+    }
+
+
+
+- (void)didReceiveMemoryWarning {
+    
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Helper methods
+
+- (void)setupDatumLabel {
+    
+    self.DatumDateFormatter = [[NSDateFormatter alloc] init];
+    [self.DatumDateFormatter setDateStyle:NSDateFormatterMediumStyle];
+    [self.DatumDateFormatter setTimeStyle:NSDateFormatterNoStyle];
+    [self.DatumDateFormatter setDateFormat:@"dd.MM.yyyy HH:mm"];
+    
+    NSDate *defaultDate = [NSDate date];
+    
+    //    self.DatumDateLabel.text = [self.dateFormatter stringFromDate:defaultDate];
+    self.DatumDateLabel.textColor = [self.tableView tintColor];
+    
+    self.selectedDatumDate = defaultDate;
+}
+
+
+
+#pragma mark - Table view methods
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    //  This is the index path of the date picker cell in the static table
+    if (indexPath.section == 0 && indexPath.row == 1 && !self.isDatumDateOpen){
+        return 0;
+    }
+    if (indexPath.section == 1 && indexPath.row == 3 && !self.GewichtPickerView.isShown){
+        return 0;
+    }
+    CGFloat height =[super tableView:tableView heightForRowAtIndexPath:indexPath];
+    return height;
+}
+
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell* cell = [tableView cellForRowAtIndexPath:indexPath];
+    if (cell == DatumCell){
+        self.DatumDateOpen = !self.isDatumDateOpen;
+        
+        if (self.datePickerIsShowing){
+            
+            [self hideDatumDatePickerCell];
+            
+        }else {
+            
+            
+            [self showDatumDatePickerCell];
+        }
+    }
+    
+    if (cell == GewichtCell) {
+        
+        if (!self.GewichtPickerView.isShown) {
+            [self.GewichtPickerView show];
+            
+        } else {
+            [self.GewichtPickerView hide];
+        }
+    }
+    
+    
+    [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+
+
+
+- (void)showDatumDatePickerCell {
+    
+    self.datePickerIsShowing = YES;
+    
+    [self.tableView beginUpdates];
+    
+    [self.tableView endUpdates];
+    
+    self.DatumDatePicker.hidden = NO;
+    self.DatumDatePicker.alpha = 0.0f;
+    
+    [UIView animateWithDuration:0.25 animations:^{
+        
+        self.DatumDatePicker.alpha = 1.0f;
+        
+    }];
+}
+
+
+- (void)hideDatumDatePickerCell {
+    
+    self.datePickerIsShowing = NO;
+    
+    [self.tableView beginUpdates];
+    [self.tableView endUpdates];
+    
+    [UIView animateWithDuration:0.25
+                     animations:^{
+                         self.DatumDatePicker.alpha = 0.0f;
+                     }
+                     completion:^(BOOL finished){
+                         self.DatumDatePicker.hidden = YES;
+                     }];
+}
+
+- (IBAction)pickerDatumDateChanged:(UIDatePicker *)sender {
+    
+    self.DatumDateLabel.text =  [self.DatumDateFormatter stringFromDate:sender.date];
+    
+    self.selectedDatumDate = sender.date;
+}
+
+
+-(void)dismissKeyboard
+{
+    UITextField *activeTextField = nil;
+    if ([GlucoseField isEditing]) activeTextField = GlucoseField;
+    else if ([BlutzuckerField isEditing]) activeTextField = BlutzuckerField;
+    else if ([HbA1cField isEditing]) activeTextField = HbA1cField;
+    if (activeTextField) [activeTextField resignFirstResponder];
+}
+
+- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if (sender != self.saveButton) return;
+    //else if did schema change? => store new data
+    
+}
+
+
+
+@end
